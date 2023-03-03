aefa9f89e9160795276bdc00fd16968547e797ee
Samuel Koszegi <koszegi.samuel@gmail.com>
2023-03-03 11:26:28 +0100

    refined version

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
-let color = "black"
+let color = "black";
 
 let click = true;
 
-function populateBoard(size){
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat( ${size}, 1fr)`;
 
-    let board = document.querySelector('.board');
-    let squares = board.querySelectorAll('div')
-    squares.forEach(div => div.remove())
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat( ${size}, 1fr)`;
-    
-let amount = size * size
+  let amount = size * size;
 
-    for (i = 0; i < amount; i++) {
-        let square = document.createElement('div')
-        square.style.border = "solid black 0.5px"
-        square.addEventListener('mouseover', colorSquare)
-        square.style.backgroundColor = "white"
-        board.insertAdjacentElement("beforeend", square)
-    }
-    
-}
-populateBoard(16)
-
-function changeSize(input){
-    if (input >= 2 || input <= 100){
-        populateBoard(input)
-    } else {
-        console.log("Error")
-    }
-}
-
-function colorSquare(){
-    if(color === 'random') {
-          this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
-    } else {
-          this.style.backgroundColor = color
-    }
-   
-   }
-
-function changeColor(choice){
-    color = choice
+  for (i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.border = "solid black 0.5px";
+    square.addEventListener("click", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
 
 
-function reset(){
-    let board = document.querySelector('.board');
-    let squares = board.querySelectorAll('div')
-    squares.forEach((div) => div.style.backgroundColor = 'white')
-    
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 || input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Error");
+  }
 }
-document.querySelector("body").addEventListener('click', () => {
-    click = !click;
+
+function colorSquare() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function reset() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
+}
+document.querySelector("body").addEventListener("click", () => {
+  click = !click;
 });
